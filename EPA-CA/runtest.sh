54ea916de54250938629e191b1e194ea76ced7c9
Fedora 27 <f27@localhost.localdomain>
2017-12-04 13:35:25 +0000

    fuxed runtest script

--- EPA-CA/runtest.sh
+++ EPA-CA/runtest.sh
@@ -4,7 +4,7 @@ for i in {1..50}
 do
 	./loadtest $i& #running loadtest in background
 	sleep 10 #sleeping for ten seconds = run time
-	echo "`mpstat -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].usr'`	$i	`mpstat -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`" >> results.dat
+	echo "`grep 'UserID \[1\]' synthetic.dat | wc -l`	$i	`mpstat -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`" >> results.dat
 	pkill loadtest #killing loadtest
 done
 
