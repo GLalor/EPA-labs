817d494ceb2e5d8e446e0a5e4b7e1053ac5cd993
Fedora 27 <f27@localhost.localdomain>
2017-12-06 14:22:10 +0000

    finished scrip 2

--- EPA-CA/runtest.sh
+++ EPA-CA/runtest.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-echo "CO	N	idle" > results.dat #header of results.dat and clears file
+echo "CO 	N 	 idle" > results.dat #header of results.dat and clears file
 for i in {1..50}
 do
 	./loadtest $i& #running loadtest in background
 	sleep 10 #sleeping for ten seconds = run time
 	#print out to results.dat containing C0 N idle time
-	echo "`grep '\[$i\]' synthetic.dat | wc -l`	$i	`mpstat -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`" >> results.dat
+	echo "`cat synthetic.dat | wc -l` 	$i 	`mpstat -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`" >> results.dat
 	pkill loadtest #killing loadtest
+	rm synthetic.dat
 done
 
