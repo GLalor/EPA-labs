f2f9ae7b5f0d5490a5a6e2333d33bffb6149f677
Graham Lalor <grahamlalor@gmail.com>
2017-12-08 14:57:16 +0000

    Update runtest.sh
    upt to 150

--- EPA-CA/runtest.sh
+++ EPA-CA/runtest.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 echo "CO 	N 	 idle" > results.dat #header of results.dat and clears file
-for i in {1..50}
+for i in {1..150}
 do
 	./loadtest $i& #running loadtest in background
-	sleep 10 #sleeping for ten seconds = run time
+	sleep 08 #sleeping for 8 seconds = run time
 	#print out to results.dat containing C0 N idle time
 	echo "`cat synthetic.dat | wc -l` 	$i 	`mpstat -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`" >> results.dat
 	pkill loadtest #killing loadtest
 	rm synthetic.dat
 done
-
